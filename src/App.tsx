25fa6e4bc78f39c7853f2785ed048fee7d269a13
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-10-08 22:39:50 +0530

    displaying the genres

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,20 @@ function App() {
     <Grid
       templateAreas={{
         base: `"nav" "main"`,
-        lg: `"nav" "nav" "aside" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '200px 1fr',
       }}
     >
       <GridItem area="nav">
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GenreList />
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem
         area="main"
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,15 +6,17 @@ import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
+  {
+    error && <Text>{error}</Text>;
+  }
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
